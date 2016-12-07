573c525e964c9eebe84b644b348c144e47ce00a6
pugakn <puga_deathnote@hotmail.com>
2016-12-07 03:27:53 -0600

    CanchaxD

--- LeagueOfSoccer/LeagueOfSoccer/LeagueOfSoccer.NativeActivity/Stage.cpp
+++ LeagueOfSoccer/LeagueOfSoccer/LeagueOfSoccer.NativeActivity/Stage.cpp
@@ -3,6 +3,7 @@
 #include "math.h"
 #include "map"
 
+
 Stage::Stage()
 {
 }
@@ -25,6 +26,8 @@ void Stage::Setup(int32_t w, int32_t h) // Inicializa el Stage
 	_walls.push_back(Wall());
 	_walls.push_back(Wall());
 	_walls.push_back(Wall());
+	_walls.push_back(Wall());
+	_walls.push_back(Wall());
 
 	_walls[0].SetScale(VECTOR4D(1,0.05f,1,1));
 	_walls[0].SetPosition(VECTOR4D(0.f,0.95f,0,1));
@@ -33,8 +36,118 @@ void Stage::Setup(int32_t w, int32_t h) // Inicializa el Stage
 
 	_walls[2].SetScale(VECTOR4D(0.05f, 1, 1, 1));
 	_walls[2].SetPosition(VECTOR4D(-0.95f, 0, 0, 1));
+
 	_walls[3].SetScale(VECTOR4D(0.05, 1, 1, 1));
 	_walls[3].SetPosition(VECTOR4D(0.95f, 0, 0, 1));
+
+	_walls[4].SetScale(VECTOR4D(0.03f, 0.20f, 1, 1));
+	_walls[4].SetPosition(VECTOR4D(-0.8,-.65f,0,1));
+	_walls[5].SetScale(VECTOR4D(0.03f, 0.20f, 1, 1));
+	_walls[5].SetPosition(VECTOR4D(0.8, -.65f, 0, 1));
+
+	_walls.push_back(Wall());
+	_walls.back().SetScale(VECTOR4D(0.03f, 0.30f, 1, 1));
+	_walls.back().SetPosition(VECTOR4D(-0.8, 0, 0, 1));
+	_walls.push_back(Wall());
+	_walls.back().SetScale(VECTOR4D(0.03f, 0.30f, 1, 1));
+	_walls.back().SetPosition(VECTOR4D(0.8, 0, 0, 1));
+
+
+	_walls.push_back(Wall());
+	_walls.back().SetScale(VECTOR4D(0.03f, 0.20f, 1, 1));
+	_walls.back().SetPosition(VECTOR4D(-0.8, 0.65f, 0, 1));
+	_walls.push_back(Wall());
+	_walls.back().SetScale(VECTOR4D(0.03f, 0.20f, 1, 1));
+	_walls.back().SetPosition(VECTOR4D(0.8, 0.65f, 0, 1));
+
+	_walls.push_back(Wall());
+	_walls.back().SetScale(VECTOR4D(0.25f, 0.03f, 1, 1));
+	_walls.back().SetPosition(VECTOR4D(0.35f, 0, 0, 1));
+	_walls.push_back(Wall());
+	_walls.back().SetScale(VECTOR4D(0.25f, 0.03f, 1, 1));
+	_walls.back().SetPosition(VECTOR4D(-0.35f, 0, 0, 1));
+
+	//Porteria bot
+	_walls.push_back(Wall());
+	_walls.back().SetScale(VECTOR4D(0.03f, 0.15f, 1, 1));
+	_walls.back().SetPosition(VECTOR4D(0.1f, -0.95f, 0, 1));
+	_walls.push_back(Wall());
+	_walls.back().SetScale(VECTOR4D(0.03f, 0.15f, 1, 1));
+	_walls.back().SetPosition(VECTOR4D(-0.1f, -0.95f, 0, 1));
+	//Porteria Top
+	_walls.push_back(Wall());
+	_walls.back().SetScale(VECTOR4D(0.03f, 0.15f, 1, 1));
+	_walls.back().SetPosition(VECTOR4D(0.1f, 0.95f, 0, 1));
+	_walls.push_back(Wall());
+	_walls.back().SetScale(VECTOR4D(0.03f, 0.15f, 1, 1));
+	_walls.back().SetPosition(VECTOR4D(-0.1f, 0.95f, 0, 1));
+
+	_walls.push_back(Wall());
+	_walls.back().SetScale(VECTOR4D(0.01f, 0.3f, 1, 1));
+	_walls.back().SetPosition(VECTOR4D(0.3f, -0.95f, 0, 1));
+	_walls.push_back(Wall());
+	_walls.back().SetScale(VECTOR4D(0.01f, 0.3f, 1, 1));
+	_walls.back().SetPosition(VECTOR4D(-0.3f, -0.95f, 0, 1));
+
+	_walls.push_back(Wall());
+	_walls.back().SetScale(VECTOR4D(0.01f, 0.3f, 1, 1));
+	_walls.back().SetPosition(VECTOR4D(0.3f, 0.95f, 0, 1));
+	_walls.push_back(Wall());
+	_walls.back().SetScale(VECTOR4D(0.01f, 0.3f, 1, 1));
+	_walls.back().SetPosition(VECTOR4D(-0.3f, 0.95f, 0, 1));
+
+	_walls.push_back(Wall());
+	_walls.back().SetScale(VECTOR4D(0.2f, 0.01f, 1, 1));
+	_walls.back().SetPosition(VECTOR4D(0.25f, -0.65f, 0, 1));
+	_walls.push_back(Wall());
+	_walls.back().SetScale(VECTOR4D(0.2f, 0.01f, 1, 1));
+	_walls.back().SetPosition(VECTOR4D(-0.25f, -0.65f, 0, 1));
+
+	_walls.push_back(Wall());
+	_walls.back().SetScale(VECTOR4D(0.2f, 0.01f, 1, 1));
+	_walls.back().SetPosition(VECTOR4D(0.25f, 0.65f, 0, 1));
+	_walls.push_back(Wall());
+	_walls.back().SetScale(VECTOR4D(0.2f, 0.01f, 1, 1));
+	_walls.back().SetPosition(VECTOR4D(-0.25f, 0.65f, 0, 1));
+
+	_walls.push_back(Wall());
+	_walls.back().SetScale(VECTOR4D(0.005f, 0.2f, 1, 1));
+	_walls.back().SetPosition(VECTOR4D(0.45f, -0.65f, 0, 1));
+	_walls.push_back(Wall());
+	_walls.back().SetScale(VECTOR4D(0.005f, 0.2f, 1, 1));
+	_walls.back().SetPosition(VECTOR4D(-0.45f, -0.65f, 0, 1));
+
+	_walls.push_back(Wall());
+	_walls.back().SetScale(VECTOR4D(0.005f, 0.2f, 1, 1));
+	_walls.back().SetPosition(VECTOR4D(0.45f, 0.65f, 0, 1));
+	_walls.push_back(Wall());
+	_walls.back().SetScale(VECTOR4D(0.005f, 0.2f, 1, 1));
+	_walls.back().SetPosition(VECTOR4D(-0.45f, 0.65f, 0, 1));
+
+	_walls.push_back(Wall());
+	_walls.back().SetScale(VECTOR4D(0.7f, 0.007f, 1, 1));
+	_walls.back().SetPosition(VECTOR4D(0, -0.20f, 0, 1));
+	_walls.push_back(Wall());
+	_walls.back().SetScale(VECTOR4D(0.7f, 0.007f, 1, 1));
+	_walls.back().SetPosition(VECTOR4D(0, 0.20f, 0, 1));
+
+	_walls.push_back(Wall());
+	_walls.back().SetScale(VECTOR4D(0.005f, 0.3f, 1, 1));
+	_walls.back().SetPosition(VECTOR4D(0.55f, 0.5f, 0, 1));
+	_walls.push_back(Wall());
+	_walls.back().SetScale(VECTOR4D(0.005f, 0.3f, 1, 1));
+	_walls.back().SetPosition(VECTOR4D(-0.55f, 0.5f, 0, 1));
+
+	_walls.push_back(Wall());
+	_walls.back().SetScale(VECTOR4D(0.005f, 0.3f, 1, 1));
+	_walls.back().SetPosition(VECTOR4D(0.55f, -0.5f, 0, 1));
+	_walls.push_back(Wall());
+	_walls.back().SetScale(VECTOR4D(0.005f, 0.3f, 1, 1));
+	_walls.back().SetPosition(VECTOR4D(-0.55f, -0.5f, 0, 1));
+
+
+
+
 	
 }
 
@@ -59,15 +172,16 @@ void Stage::Update()
 			VECTOR4D plano = _walls[i]._triangles[j].GetPlane();
 			VECTOR4D planoMod(plano);
 			VECTOR4D rad = _ball->_mesh.m_Vertices[0] - _ball->_position;
-			float radius = Magnity(rad);
+			float radius = 0.02f;//Magnity(rad);
 			planoMod.w -= radius;
 			VECTOR4D nuevaPos = _ball->_position + _ball->_velocity;
-			if ((Dot(_ball->_position - (0.005* dir), planoMod) > 0) && (Dot(nuevaPos + (0.005* dir), planoMod) < 0)) {
+			if ((Dot(_ball->_position - (0.005f* dir), planoMod) > 0) && (Dot(nuevaPos + (0.005f* dir), planoMod) < 0)) {
 				float denom = Dot(planoMod, dir);
 				if (denom == 0) continue;
 				float t = (Dot(planoMod, _ball->_position)) / denom;
 				VECTOR4D pDeTe = _ball->_position + t*dir;
-				VECTOR4D C = pDeTe + radius * _walls[i]._triangles[j].GetNormal();
+				if (!PtInTriangle(_walls[i]._triangles[j], pDeTe)) continue;
+				VECTOR4D C = pDeTe - radius * _walls[i]._triangles[j].GetNormal();
 				C.z = 0;
 				VECTOR4D dist = pDeTe - _ball->_position + (_walls[i]._triangles[j].GetNormal()* 0.003f);
 				_ball->_position = _ball->_position + dist;
